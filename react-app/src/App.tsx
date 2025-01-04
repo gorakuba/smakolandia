33078a7d9be9943a697630982536250d8470c046
gorakuba <gorakuba083@gmail.com>
2025-01-04 10:30:48 +0100

    fix

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,39 @@
 import { hot } from 'react-hot-loader/root'
 import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+type Recipe = {
+  id: number
+  name: string
+  ingredients: string[]
+}
 
 function App() {
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+
+  useEffect(() => {
+    axios
+      .get('/api/recipes')
+      .then((response) => {
+        setRecipes(response.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching recipes:', error)
+      })
+  }, [])
+
   return (
-    <div className='App'>
-      <header className='App-header'>Welcome to smakolandia!</header>
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <p>Ingredients: {recipe.ingredients.join(', ')}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
